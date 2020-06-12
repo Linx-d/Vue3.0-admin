48094690ec64985d79d6ca64f948924c004dc1dc
lixin <q1404433837@163.com>
2020-06-12 14:28:48 +0800

    [feat] 调试拦截器

--- src/views/contacts/index.vue
+++ src/views/contacts/index.vue
@@ -1,19 +1,33 @@
 <template>
   <main id="contacts" class="frame_center">
     <div class="contacts_main frame_center_main">
-      <div class="chunk_title">
-        contacts
-      </div>
+      <div class="chunk_title">contacts</div>
       <div class="chunk_cnt">
-
+        <el-button :plain="true" @click="open4">错误</el-button>
       </div>
     </div>
   </main>
 </template>
 
 <script>
+import { getSms } from "@/api/loginApi";
+import { onMounted } from "@vue/composition-api";
+export default {
+  name: "contacts",
+  setup(props, { root }) {
+    let open4 = () => {
+      getSms(1).then(response => {
+        root.$message.success(response.msg);
+        console.log(response);
+      });
+    };
+    onMounted(() => {
+      console.log("????");
+    });
+    return { open4 };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
