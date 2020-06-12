1dd42aa9bda79781bfe8b54611953e6072fa515c
lixin <q1404433837@163.com>
2020-06-12 17:45:32 +0800

    [delete] 删除

--- src/api/loginApi.js
+++
@@ -1,8 +0,0 @@
-import service from "@/utils/request";
-export function getSms(data) {
-  // 为给定 ID 的 user 创建请求
-  return service.request({
-    method: "post",
-    url: "/dept/listUserByDepartment?id=" + data
-  });
-}
