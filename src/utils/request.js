0a43b24b60905d2ef89e9575deb13a416f8f8009
lixin <q1404433837@163.com>
2020-06-12 17:42:19 +0800

    [test] “local添加请求头 auth_token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,36 +1,45 @@
 import axios from "axios";
-import { Message } from 'element-ui'; // 单独引入 Message：
+import { Message } from "element-ui"; // 单独引入 Message：
 //使用自定义配置新建一个 axios 实例
 const service = axios.create({
-  baseURL: "http://iot.chinautech.com/api",
-  timeout: 1000
+  baseURL: "http://192.168.0.192:8888/api",
+  timeout: 1000,
+  headers: {
+    auth_token: "utechqm4CH4kSc8fwleV18AZBig"
+  },
 });
 
 // 添加请求拦截器
-service.interceptors.request.use(function (config) {
-  // 在发送请求之前做些什么
-  return config;
-}, function (error) {
-  // 对请求错误做些什么
-  return Promise.reject(error);
-});
+service.interceptors.request.use(
+  (config) => {
+    // 在发送请求之前做些什么
+    return config;
+  },
+  (error) => {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  }
+);
 
 // 添加响应拦截器
-service.interceptors.response.use(function (response) {
-  // 对响应数据做点什么
-  let data = response.data;
-  if(data.code !== 0) {
-    console.log(data);
-    Message.error(data.msg);
-    return Promise.reject(data);
-  }else {
-    //return response; //所有相应数据
-    console.log(data);
-    Message.success(data.msg);
-    return Promise.resolve(data); //只有data数据
+service.interceptors.response.use(
+  function(response) {
+    // 对响应数据做点什么
+    let data = response.data;
+    if (data.code !== 0) {
+      console.log(data);
+      //Message.error(data.msg);
+      return Promise.reject(data);
+    } else {
+      //return response; //所有相应数据
+      console.log(data);
+      //Message.success(data.msg);
+      return Promise.resolve(data); //只有data数据
+    }
+  },
+  function(error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
   }
-}, function (error) {
-  // 对响应错误做点什么
-  return Promise.reject(error);
-});
+);
 export default service;
