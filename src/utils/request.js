d948e0b6e306112d11eaa9dd485429f3202d0010
lixin <q1404433837@163.com>
2020-06-12 14:30:05 +0800

    [feat] 调试拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Message } from 'element-ui'; // 单独引入 Message：
 //使用自定义配置新建一个 axios 实例
 const service = axios.create({
   baseURL: "http://iot.chinautech.com/api",
@@ -17,7 +18,17 @@ service.interceptors.request.use(function (config) {
 // 添加响应拦截器
 service.interceptors.response.use(function (response) {
   // 对响应数据做点什么
-  return response;
+  let data = response.data;
+  if(data.code !== 0) {
+    console.log(data);
+    Message.error(data.msg);
+    return Promise.reject(data);
+  }else {
+    //return response; //所有相应数据
+    console.log(data);
+    Message.success(data.msg);
+    return Promise.resolve(data); //只有data数据
+  }
 }, function (error) {
   // 对响应错误做点什么
   return Promise.reject(error);
