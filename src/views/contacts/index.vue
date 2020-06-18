1e7574de8ba61c5b08fd28081ece126835388558
lixin <q1404433837@163.com>
2020-06-18 19:09:20 +0800

    [feat] 部门管理界面功能实现：展示部门信息，修改部门名称、删除部门、添加子部门、将未分组用户添加到顶级部门

--- src/views/contacts/index.vue
+++ src/views/contacts/index.vue
@@ -1,7 +1,110 @@
 <template>
   <main id="contacts" class="frame_center">
     <div class="contacts_main frame_center_main">
-      <div class="chunk_title">contacts</div>
+      <div class="chunk_title">
+        <div class="chunk_title_top">
+          <el-input class="search" placeholder="搜索部门" v-model="filterText"></el-input>
+          <a href="javascript:;" class="addChildMember">
+            <i>+</i>
+          </a>
+        </div>
+        <div class="chunk_title_bottom">
+          <el-tree
+            class="filter-tree memberList"
+            :data="memberData"
+            :props="defaultProps"
+            node-key="id"
+            :filter-node-method="filterNode"
+            @node-click="handleNodeClick"
+            :expand-on-click-node="false"
+            ref="tree"
+          >
+            <span class="custom-tree-node" slot-scope="{ node, data }">
+              <span class="tree-text">{{ node.label }}</span>
+              <span>
+                <!--
+                <el-button type="text" size="mini" @click="() => append(data)">Append</el-button>
+                <el-button type="text" size="mini" @click="() => remove(node, data)">Delete</el-button>
+                -->
+                <el-button
+                  type="text"
+                  @click.stop="dialogShow(data, node)"
+                  class="addMemberBtn"
+                >id:{{data.id}} +</el-button>
+                <el-button
+                  type="text"
+                  size="mini"
+                  @click.stop="dialogDel(data, node)"
+                  class="delMemberBtn"
+                >-</el-button>
+                <el-button
+                  type="text"
+                  size="mini"
+                  @click.stop="dialogmodify(data, node)"
+                  class="modifyMemberBtn"
+                >*</el-button>
+                <el-dialog title="添加部门" :visible.sync="dialogFormVisible" class="addDialog">
+                  <el-form :model="form">
+                    <el-form-item label="部门名称" :label-width="formLabelWidth">
+                      <el-input
+                        v-model="form.name"
+                        autocomplete="off"
+                        ref="partmentContent"
+                        maxlength="30"
+                        placeholder="请输入部门名称"
+                      ></el-input>
+                    </el-form-item>
+                  </el-form>
+                  <div slot="footer" class="dialog-footer">
+                    <el-button @click.stop="dialogFormVisible = false">取 消</el-button>
+                    <el-button
+                      type="primary"
+                      @click.stop="dialogHide(childData)"
+                      :disabled="addStatus"
+                    >确 定</el-button>
+                  </div>
+                </el-dialog>
+
+                <el-dialog title="修改部门名称" :visible.sync="modifyData.visibel" class="modifyDialog">
+                  <el-form :model="modifyData">
+                    <el-form-item label="部门名称" :label-width="formLabelWidth">
+                      <el-input
+                        v-model="modifyData.name"
+                        autocomplete="off"
+                        ref="partmentChangeContent"
+                        maxlength="30"
+                        :placeholder="modifyData.name"
+                      ></el-input>
+                    </el-form-item>
+                  </el-form>
+                  <div slot="footer" class="dialog-footer">
+                    <el-button @click.stop="modifyData.visibel = false">取 消</el-button>
+                    <el-button
+                      type="primary"
+                      @click.stop="dialogModifyHide()"
+                      :disabled="modifyData.status"
+                    >保 存</el-button>
+                  </div>
+                </el-dialog>
+
+                <el-dialog
+                  title="移除部门"
+                  :visible.sync="dialogVisible"
+                  width="30%"
+                  :before-close="handleClose"
+                  class="addDialog"
+                >
+                  <span>{{ memberLabel }}</span>
+                  <span slot="footer" class="dialog-footer">
+                    <el-button @click.stop="dialogVisible = false">取 消</el-button>
+                    <el-button type="primary" @click.stop="dialogDelHide()">确 定</el-button>
+                  </span>
+                </el-dialog>
+              </span>
+            </span>
+          </el-tree>
+        </div>
+      </div>
       <div class="chunk_cnt">
         <el-button :plain="true" @click="open4">成功</el-button>
       </div>
@@ -19,60 +122,437 @@ import {
   listUserByDepartment,
   listUserByNoDepartment,
   removeMember,
-  listDepartmentByPid
+  listDepartmentByPid,
+  translateDataToTree,
+  getObjLevel
 } from "@/api/contactsApi";
-import { onMounted } from "@vue/composition-api";
+import sha1 from "sha1"; //前台加密 使用:  sha1(value);
+import cookie from "cookie_js";
+import {
+  onMounted,
+  reactive,
+  watchEffect,
+  ref,
+  computed
+} from "@vue/composition-api";
 export default {
   name: "contacts",
-  setup(props, { root }) {
-    let open4 = () => {
+  setup(props, { root, refs, set }) {
+    /**
+     * 数据
+     */
+    let companyData = reactive({
+      companyId: 1
+    }); // 顶级部门 id
+    let memberLabel = ref("移除部门");
+    let memberData = reactive([
+      {
+        label: "",
+        displayOrder: 10001,
+        id: 1,
+        pid: 0,
+        children: [],
+        childrenLen: 0
+      }
+    ]);
+    //selectChildDepart(0);
+    let childData = reactive({}); // add child
+    let addStatus = ref(false); // 增加部门的确定按钮的禁用状态
+    let removeData = reactive({
+      data: {},
+      node: {}
+    }); // remove child
+    let modifyNodeData = reactive({});
+    const defaultProps = reactive({
+      children: "children",
+      label: "label"
+    });
+    const filterText = ref("");
+    const dialogFormVisible = ref(false); // formLog
+    const dialogVisible = ref(false); // delLog
+    const form = reactive({
+      name: "",
+      delivery: false,
+      type: [],
+      resource: "",
+      desc: ""
+    });
+    const modifyData = reactive({ name: "", visibel: false, status: false }); // 修改部门名称 data
+    const formLabelWidth = ref("100px");
+    /**
+     * 函数
+     *
+     */
+
+    /**
+     * 添加子部门 btn
+     */
+    const append = data => {
+      addDepart(form.name, data.id).then(response => {
+        let resData = response.data;
+        const newChild = {
+          id: resData.id,
+          pid: resData.pid,
+          label: resData.name,
+          displayOrder: resData.displayOrder,
+          children: []
+        };
+        if (!data.children) {
+          set(data, "children", []);
+        }
+        data.children.push(newChild);
+        root.$message({
+          message: "成功添加部门",
+          type: "success"
+        });
+      });
+    };
+
+    /**
+     * 删除子部门 btn
+     */
+    const remove = (node, data) => {
+      let id = data.id;
+      delDepart(id)
+        .then(response => {
+          const parent = node.parent;
+          const children = parent.data.children || parent.data;
+          const index = children.findIndex(d => d.id === data.id);
+          children.splice(index, 1);
+          root.$message({
+            message: "成功移除部门",
+            type: "success"
+          });
+        })
+        .catch(response => {
+          root.$message({
+            message: response.msg,
+            type: "error"
+          });
+        });
+    };
+
+    /**
+     * 查询子部门
+     */
+    const selectChildDepart = id => {
+      let listPidParams = new URLSearchParams();
+      listPidParams.append("id", id);
+      listDepartmentByPid(listPidParams).then(response => {
+        let data = response.data;
+        let len = data.length,
+          i = 0;
+        for (i; i < len; i++) {
+          let currentMember = data[i];
+          console.log(currentMember);
+        }
+      });
+    };
+
+    /**
+     * 添加部门成员
+     */
+    const addMemberFn = (depId, userIdArr) => {
+      let params = new URLSearchParams();
+      params.append("userId", userIdArr); // [1, 2, 7]
+      params.append("depId", depId); // 7
+      return addMember(params);
+    };
+
+    /**
+     * 查询未分组用户
+     */
+    const selectNodepart = () => {
+      return listUserByNoDepartment();
+    };
+
+    /**
+     * 查询所有部门
+     */
+    const selectAllDepart = () => {
+      listAllDepartment()
+        .then(response => {
+          let data = response.data,
+            len = data.length;
+          if (len <= 0) {
+            root.$message({ message: "暂无部门" });
+          } else {
+            let id = data[0].id;
+            companyData.companyId = id;
+            memberData[0].id = id;
+            let treeData = translateDataToTree(data)[0];
+            if (treeData == undefined) {
+              root.$message({
+                message: "部门信息有误",
+                type: "error"
+              });
+            } else {
+              memberData[0].label = treeData.name;
+              memberData[0].children = treeData.children;
+              memberData[0].childrenLen = treeData.childrenLen;
+              memberData[0].displayOrder = treeData.displayOrder;
+              memberData[0].id = treeData.id;
+              memberData[0].pid = treeData.pid;
+            }
+          }
+          return companyData;
+        })
+        .then(response => {
+          let id = response.companyId;
+          selectNodepart().then(response => {
+            let data = response.data,
+              len = data.length;
+            if (len !== 0) {
+              let companyId = id,
+                userIdArr = [],
+                i = 0;
+              for (i; i < len; i++) {
+                let user = data[i];
+                userIdArr.push(user.userId);
+              } 
+              addMemberFn(companyId, userIdArr).then(response => {
+                //console.log(response);// 将未分组用户自动加入顶级部门--添加成功
+              });
+            } else {
+              //暂无为分组用户
+              return;
+            }
+          });
+        });
+    };
+
+    /**
+     * 添加部门
+     */
+    const addDepart = (name, pid) => {
+      return addDepartment({
+        name: name,
+        pid: pid
+      });
+    };
+
+    /**
+     * 删除部门
+     */
+    const delDepart = id => {
+      let removeDepartmentId = new URLSearchParams();
+      removeDepartmentId.append("id", id);
+      return removeDepartment(removeDepartmentId);
+    };
+
+    /**
+     * 修改部门信息
+     */
+    const updateDepart = modifyNodeData => {
+      let data = modifyNodeData.data;
+      return updateDepartment({
+        id: data.id,
+        name: data.label,
+        pid: data.pid,
+        displayOrder: data.displayOrder
+      });
+    };
+
+    /**
+     * memberData 点击事件
+     */
+    const handleNodeClick = data => {
+      //console.log(data);
+    };
+
+    /**
+     * 过滤
+     */
+    const filterNode = (value, data) => {
+      if (!value) return true;
+      return data.label.indexOf(value) !== -1;
+    };
+
+    /**
+     * 成功提示函数
+     */
+    const open4 = () => {
       //查询部门成员
       let listUserId = new URLSearchParams();
       listUserId.append("id", 7);
-      listUserByDepartment(listUserId)
-      .then(response => {
-        root.$message.success(response.msg);
+      listUserByDepartment(listUserId).then(response => {
+        root.$message({
+          message: response.msg,
+          type: "warning"
+        });
       });
+      selectAllDepart();
+    };
+    /**
+     * handleClose
+     */
+    const handleClose = done => {
+      this.$confirm("确认关闭？")
+        .then(() => {
+          done();
+        })
+        .catch(() => {});
+    };
+
+    /**
+     * dialog 显示 btn
+     * type: 添加部门
+     */
+    const dialogShow = (data, node) => {
+      let len = node.level;
+      if (len > 14) {
+        root.$message({
+          message: "子部门层级超过15层，无法添加"
+        });
+      } else {
+        childData = data;
+        dialogFormVisible.value = true;
+      }
+    };
+    /**
+     * dialog 隐藏 btn
+     * type: 添加部门
+     */
+    const dialogHide = () => {
+      let inputTxt = form.name,
+        len = inputTxt.length;
+      if (len !== 0) {
+        dialogFormVisible.value = false;
+        append(childData);
+      } else {
+        addStatus.value = true;
+        root.$message({
+          message: "部门名称不能为空",
+          type: "warning"
+        });
+      }
+    };
+    /**
+     * dialogDel 显示 btn
+     * type: 移除部门
+     */
+    const dialogDel = (data, node) => {
+      removeData = {
+        data: data,
+        node: node
+      };
+      let id = removeData.data.id || 0;
+      memberLabel.value = `是否移除'${removeData.data.label}'`;
+      if (companyData.companyId == id) {
+        root.$message({
+          message: "不能移除顶级部门",
+          type: "warning"
+        });
+      } else {
+        dialogVisible.value = true;
+      }
+    };
+    /**
+     * dialogDelHide 隐藏 btn
+     * type: 移除部门
+     */
+    const dialogDelHide = () => {
+      dialogVisible.value = false;
+      remove(removeData.node, removeData.data);
+      delDepart();
+    };
+    /**
+     * dialogmodify 显示 btn
+     * type: 修改部门名称
+     */
+    const dialogmodify = (data, node) => {
+      let pid = data.pid;
+      if (pid !== 0) {
+        modifyNodeData = node;
+        modifyData.name = data.label;
+        modifyData.visibel = true;
+      } else {
+        root.$message({
+          message: "不能更改顶级部门的名称",
+          type: "warning"
+        });
+      }
+    };
+    /**
+     * dialogModifyHide 隐藏 btn
+     * type: 修改部门名称
+     */
+    const dialogModifyHide = () => {
+      let len = modifyData.name.length;
+      if (len <= 0) {
+        root.$message({
+          message: "部门名称不能为空",
+          type: "warning"
+        });
+        modifyData.status = true;
+      } else {
+        let inputTxt = refs.partmentChangeContent.value;
+        modifyNodeData.data.label = inputTxt;
+        modifyNodeData.data.name = inputTxt;
+        updateDepart(modifyNodeData)
+          .then(response => {
+            root.$message({
+              message: "修改名称成功",
+              type: "success"
+            });
+            modifyData.visibel = false;
+          })
+          .catch(response => {
+            root.$message({
+              message: response.msg,
+              type: "error"
+            });
+          });
+      }
     };
     onMounted(() => {
-      console.log("挂载完成");
-
-      //查询所有部门
-      listAllDepartment();
-
-
-      //添加部门
-      addDepartment({
-        name: "部门名",
-        pid: 7
+      console.log("挂载完成"); //addMemberFn selectNodepart
+      selectAllDepart();
+      /**在挂载完成之后在执行侦听器 */
+      watchEffect(
+        () => filterText.value,
+        val => {
+          refs.tree.filter(val);
+        },
+        () => memberData
+      );
+      watchEffect(() => {
+        //dialogFormVisible modifyData.visibel
+        if (dialogFormVisible.value === false) {
+          form.name = "";
+        }
       });
-
-      //修改部门信息
-      updateDepartment({
-        id: 7,
-        name: "修改部门名字",
-        pid: 0,
-        displayOrder: 10001
+      watchEffect(() => {
+        let len = form.name.length;
+        if (len > 0) {
+          addStatus.value = false;
+        } else if (len >= 30) {
+          root.$message({
+            message: "部门名称长度不能大于30",
+            type: "warning"
+          });
+        }
       });
+      watchEffect(() => {
+        let modifyLen = modifyData.name.length;
+        if (modifyLen > 0) {
+          modifyData.status = false;
+        } else if (modifyLen >= 30) {
+          root.$message({
+            message: "部门名称长度不能大于30",
+            type: "warning"
+          });
+        }
+      });
+      /*
 
-      //添加部门成员
-      let params = new URLSearchParams();
-      params.append("userId", [1, 2, 7]);
-      params.append("depId", 7);
-      addMember(params);
-
-      //删除部门
-      let removeDepartmentId = new URLSearchParams();
-      removeDepartmentId.append("id", 72);
-      //removeDepartment(removeDepartmentId);
-
+      
       //查询部门成员
       let listUserId = new URLSearchParams();
       listUserId.append("id", 7);
       listUserByDepartment(listUserId);
 
-      //查询未分组用户
-      listUserByNoDepartment();
+      
 
       //删除部门成员
       let removeMemberParams = new URLSearchParams();
@@ -81,16 +561,83 @@ export default {
       removeMember(removeMemberParams);
 
       //移动部门成员
-
-      //查询子部门
-      let listPidParams = new URLSearchParams();
-      listPidParams.append("id", 1);
-      listDepartmentByPid(listPidParams);
+*/
     });
-    return { open4 };
+
+    return {
+      open4,
+      memberData,
+      defaultProps,
+      handleNodeClick,
+      filterNode,
+      filterText,
+      append,
+      remove,
+      //dialog
+      dialogFormVisible,
+      form,
+      formLabelWidth,
+      childData,
+      dialogHide,
+      dialogShow,
+      // delDialog
+      dialogVisible,
+      dialogDel,
+      dialogDelHide,
+      handleClose,
+      removeData,
+      addStatus,
+      memberLabel,
+      //modify
+      modifyData,
+      dialogmodify,
+      dialogModifyHide
+    };
   }
 };
 </script>
 
 <style lang="scss" scoped>
+$contactsHeight: 592px;
+#contacts {
+  .contacts_main {
+    .chunk_title {
+      width: 257px;
+      max-width: 257px;
+      height: $contactsHeight;
+      .chunk_title_top {
+        padding: 10px;
+        border-bottom: #e4e6e9;
+        position: relative;
+        .search {
+          width: 212px;
+        }
+        .addChildMember {
+          width: 20px;
+          height: 20px;
+          text-align: center;
+          font-size: 27px;
+          color: #aaaaaa;
+          position: absolute;
+          right: 4px;
+          top: 9px;
+          i {
+            font-style: normal;
+          }
+        }
+      }
+      .chunk_title_bottom {
+        .el-tree-node__children {
+          max-width: 257px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+      }
+    }
+    .chunk_cnt {
+      height: $contactsHeight;
+    }
+  }
+}
 </style>
