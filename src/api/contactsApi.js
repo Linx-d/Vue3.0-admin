caed1b91376f7434d08a13c6f1bfb7fa7ff727cd
lixin <q1404433837@163.com>
2020-07-01 17:42:02 +0800

    [feat] 抽离工具函数，新增获取用户接口函数

--- src/api/contactsApi.js
+++ src/api/contactsApi.js
@@ -111,10 +111,10 @@ export function listUserByDepartment(data) {
  * @param
  * @returns
  */
-export function listUserByNoDepartment() {
+export function listUserByNoDepartment(pageNum, pageSize) {
   return service.request({
     method: "get",
-    url: "/dept/listUserByNoDepartment",
+    url: "/dept/listUserByNoDepartment?pageNum=" + pageNum + "&pageSize=" + pageSize
   });
 }
 
@@ -148,67 +148,35 @@ export function listDepartmentByPid(data) {
   });
 }
 
+/**---------------------------------------- 用户管理 ----------------------------------------**/
 /**
+ * 根据用户id获取历史数据信息
  *
- * 树状数组
+ * @export
+ * @param {
+    "userId": 49
+  }
+ * @returns
  */
-export function translateDataToTree(data, parentId = 0) {
-  let tree = [];
-  let temp;
-  data.forEach((item, index) => {
-    if (data[index].pid == parentId) {
-      let obj = data[index];
-      temp = translateDataToTree(data, data[index].id);
-      obj["label"] = obj["name"];
-      if (temp.length > 0) {
-        obj.children = temp;
-        obj.childrenLen = obj.children.length;
-      } else {
-        obj.children = [];
-        obj.childrenLen = obj.children.length;
-      }
-      tree.push(obj);
-    }
+export function listUserLocationById(data) {
+  return service.request({
+    method: "post",
+    url: "/deviceData/listUserLocationById?page=1&limit=10",
+    data
   });
-  return tree;
-}
-/**求数组的深度
- *
- */
-export function getType(param) {
-  return Object.prototype.toString
-    .call(param)
-    .slice(8, -1)
-    .toLowerCase();
 }
 
-export function getObjLevel(obj) {
-  /* 需要忽略数组时使用
-  if(getType(obj) !== 'object'){
-        throw new Error('paramater must be object')
-    }
-  */
-  // 用来保存结果，初始化层级为0
-  let res = 0;
-  function loopGetLevel(obj, level = res) {
-    //当前数据是不是对象，是对象就继续，否则比较下层级和值，哪个大取哪个
-    if (typeof obj === "object") {
-      //对对象的每个属性进行遍历，如果还是对象就递归计算，否则就比较res和level取最大值
-      for (var key in obj) {
-        if (typeof obj === "object") {
-          if (Array.isArray(obj)) {
-            loopGetLevel(obj[key], level); // 注意这里，当是数组时继续遍历，但是层数不增加
-          } else {
-            loopGetLevel(obj[key], level + 1);
-          }
-        } else {
-          res = level + 1 > res ? level + 1 : res;
-        }
-      }
-    } else {
-      res = level > res ? level : res;
-    }
-  }
-  loopGetLevel(obj);
-  return res;
+/**
+ * 根据用户id获取设备最新数据和告警信息
+ *
+ * @export
+ * @param [1, 2]
+ * @returns
+ */
+export function listDeviceAlarmInfoByUserId(data) {
+  return service.request({
+    method: "post",
+    url: "/device/listDeviceAlarmInfoByUserId",
+    data
+  });
 }
