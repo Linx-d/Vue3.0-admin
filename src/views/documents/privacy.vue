d86168a599d1c26714ff0d5fc56f03808646db9e
lixin <a1404433837@163.com>
2020-07-29 15:46:02 +0800

    [feat] 文档：隐私政策

---
+++ src/views/documents/privacy.vue
@@ -0,0 +1,21 @@
+<template>
+  <main id="privacy" class="frame_center document_fix">
+    <div>
+      <div class="privacy_main frame_center_main frame_cnt">
+        <h1>privacy</h1>
+      </div>
+      <p class="version">© 2009&nbsp;-&nbsp;2020&nbsp;Utech Inc. All Rights Reserved</p>
+    </div>
+  </main>
+</template>
+<script>
+export default {
+  name: "privacy",
+  setup() {},
+};
+</script>
+<style lang="scss" scoped>
+.version {
+  margin-top: 24px;
+}
+</style>
