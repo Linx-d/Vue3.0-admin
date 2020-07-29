584c87742b30cfcda3b5908158d716101af379cd
lixin <a1404433837@163.com>
2020-07-29 15:46:29 +0800

    [feat] 文档：更新日志

---
+++ src/views/documents/updateLog.vue
@@ -0,0 +1,21 @@
+<template>
+  <main id="updateLog" class="frame_center document_fix">
+    <div>
+      <div class="updateLog_main frame_center_main frame_cnt">
+        <h1>updateLog</h1>
+      </div>
+      <p class="version">© 2009&nbsp;-&nbsp;2020&nbsp;Utech Inc. All Rights Reserved</p>
+    </div>
+  </main>
+</template>
+<script>
+export default {
+  name: "updateLog",
+  setup() {},
+};
+</script>
+<style lang="scss" scoped>
+.version {
+  margin-top: 24px;
+}
+</style>
