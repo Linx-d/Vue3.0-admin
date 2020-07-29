eed1d0e1502f10112d7666fb5ccf3b9cfb5c5dd0
lixin <a1404433837@163.com>
2020-07-29 15:42:34 +0800

    [update] 地图模块: 1.统计信息、分为在线人数、体温异常人数统计

--- src/views/map/options/onlineOption.js
+++ src/views/map/options/onlineOption.js
@@ -1,41 +1,40 @@
 const online = {
-  title: {
-      text: '统计',
-      subtext: '信息',
-      left: 'center'
-  },
-  tooltip: {
-      trigger: 'item',
-      formatter: '{a} <br/>{b} : {c} ({d}%)'
-  },
-  legend: {
-      orient: 'vertical',
-      left: 'left',
-      data: ['体温异常', '总人数', '在线人数', '低电量人数']//['手环脱落', '总人数', '设备故障', '体温异常', '在线人数']
-  },
-  series: [
-      {
-          name: '访问来源',
-          type: 'pie',
-          radius: '55%',
-          center: ['50%', '60%'],
-          data: [
-            //   {value: 335, name: '手环脱落'},
-              {value: 310, name: '体温异常'},
-            //   {value: 234, name: '设备故障'},
-              {value: 135, name: '总人数'},
-              {value: 1548, name: '在线人数'},
-              {value: 1548, name: '低电量人数'}
-          ],
-          emphasis: {
-              itemStyle: {
-                  shadowBlur: 10,
-                  shadowOffsetX: 0,
-                  shadowColor: 'rgba(0, 0, 0, 0.5)'
-              }
-          }
-      }
-  ]
+    title: {
+        text: '统计',
+        subtext: '信息',
+        left: 'center'
+    },
+    tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)'
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['体温异常人数', '体温正常人数']
+    },
+    series: [{
+        name: '访问来源',
+        type: 'pie',
+        radius: '55%',
+        center: ['50%', '60%'],
+        data: [{
+                value: 0,
+                name: '体温正常人数'
+            },
+            {
+                value: 0,
+                name: '体温异常人数'
+            }
+        ],
+        emphasis: {
+            itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+        }
+    }]
 };
 
 export default online;
