608a628bb94d4fcb1e3d933a5f43c8f76e9d3ad7
lixin <a1404433837@163.com>
2020-07-29 15:45:10 +0800

    [feat] 文档：用户协议

---
+++ src/views/documents/userAgreement.vue
@@ -0,0 +1,40 @@
+<template>
+  <main id="userAgreement" class="frame_center document_fix">
+    <div>
+      <div class="userAgreement_main frame_center_main frame_cnt">
+        <p class="MsoNormal"><span><b>如泰科技疫情防控中心许可及服务协议</b></span></p>
+        <p class="MsoNormal"><span><b>首部及导言</b></span></p>
+        <p class="MsoNormal"><span>欢迎使用如泰科技疫情防控中心平台及服务！</span></p>
+        <p class="MsoNormal"><span>为使用如泰科技疫情防控中心服务平台（以下简称“本平台”）及服务，
+          用户应当阅读并遵守《如泰科技疫情防控中心许可及服务协议》（以下简称“本协议”）。请用户务必审慎阅读、
+          充分理解各条款内容，特别是免除或者限制责任的条款，以及开通或使用某项服务的单独协议，并选择接受或不接受。
+          限制、免责条款可能以加粗形式提示用户注意。</span></p>
+        <p class="MsoNormal"><span>除非用户已阅读并接受本协议所有条款，否则用户无权使用本软件及相关服务。用户的登录、使用等行为即视为用户已阅读并同意上述协议的约束。</span></p>
+        <p class="MsoNormal"><span><b>一、协议的范围</b></span></p>
+        <p class="MsoNormal"><span>本协议是用户与如泰科技之间关于用户使用、复制本平台，以及使用如泰科技相关服务所订立的协议。“如泰科技”是指如泰科技公司及其相关服务可能存在的运营关联单位。“疫情防控中心用户”是指登录、使用疫情防控中心平台及服务并获得管理权限的个人或组织，在本协议中更多地称为“你”。“最终用户”是指收到邀请后加入你所创建的疫情防控中心的个人用户。前述“疫情防控中心用户”及“最终用户”统称为“用户”或“所有用户”。</span></p>
+        <p class="MsoNormal"><span><b>二、关于本服务</b></span></p>
+        <p class="MsoNormal"><span>2.1 本服务的内容</span></p>
+        <p class="MsoNormal"><span>本服务内容是指如泰向用户提供的疫情防控管理工具（以下简称“疫情防控”），支持单人、多人参与，在监测成员体温、位置等即时信息服务的基础上，同时为用户提供包括但不限于组织成员管理以及数据分析等服务（以下简称“本服务”）。</span></p>
+        <p class="MsoNormal"><span>2.2 本服务的形式</span></p>
+        <p class="MsoNormal"><span>2.2.1 用户使用本服务需要注册企业微信帐号并下载企业微信客户端软件，腾讯将向用户提供企业微信的管理权限、使用功能和使用说明等服务。如泰科技疫情防控将通过企业微信账号进行登录或在企业微信移动端软件进入，并尽应有之努力保障本服务的正常运行。</span></p>
+      </div>
+      <p class="version">© 2009&nbsp;-&nbsp;2020&nbsp;Utech Inc. All Rights Reserved</p>
+    </div>
+  </main>
+</template>
+<script>
+export default {
+  name: "userAgreement",
+  setup() {},
+};
+</script>
+<style lang="scss" scoped>
+.userAgreement_main {
+  p:first-child {
+    font-size: 20px;
+  }
+}
+.version {
+  margin-top: 24px;
+}
+</style>
