cd6b3c6a77252278be8927ffd45cd503979eef24
lixin <a1404433837@163.com>
2020-07-29 15:45:37 +0800

    [feat] 文档:：使用手册

---
+++ src/views/documents/manual.vue
@@ -0,0 +1,21 @@
+<template>
+  <main id="manual" class="frame_center document_fix">
+    <div>
+      <div class="manual_main frame_center_main frame_cnt">
+        <p class="MsoNormal"><span>manual</span></p>
+      </div>
+      <p class="version">© 2009&nbsp;-&nbsp;2020&nbsp;Utech Inc. All Rights Reserved</p>
+    </div>
+  </main>
+</template>
+<script>
+export default {
+  name: "manual",
+  setup() {},
+};
+</script>
+<style lang="scss" scoped>
+.version {
+  margin-top: 24px;
+}
+</style>
